d36f915f1566344a612a1b4d92a5471df3706967
Opilane TTHK <opilane@tthk.ee>
2020-03-05 12:53:43 +0200

    commmit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname+ '/index.html');
+});
+
+app.post('/', function(req, res){
+    let currency = req.body.currency;
+    let url = `https://api.coindesk.com/v1/bpi/currentprice/${currency}.json`;
+    //console.log(currency);
+    //res.send(`you have selected ${currency}`);
+
+    request(url, function(error, response, body){
+        console.log("Status Message: ", response.statusMessage);
+        console.log("Server Status Code", response.statusCode);
+        console.log(response.body);
+
+        let data = JSON.parse(response.body);
+        let price;
+
+        if(currency === "EUR"){
+            price = data.bpi.EUR.rate;
+            console.log("Price in EUR", price);
+            
+        } else{
+            price = data.bpi.USD.rate_float;
+            console.log("Pirce in USD", price);
+            
+        }
+
+        let disclaimer = data.disclaimer;
+
+        res.write(`${disclaimer}`);
+        res.write('<br>');
+        res.write(`Current price in ${currency} is ${price}`);
+        res.send();
+        
+        
+        
+    });
+    
+});
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+    
+});
